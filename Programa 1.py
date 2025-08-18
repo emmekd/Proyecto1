a2ccc4ba2b88bc5d5ecbcc654891773afb4a5bac
JorgeLuisVG <jorgeluisvg0500@gmail.com>
2025-08-18 16:24:10 -0600

    base del menu

--- Programa 1.py
+++ Programa 1.py
@@ -3,11 +3,20 @@ print("Cajero Automatico")
 
 while True:
     print("Ingrese una opcion")
-    print("Retirar dinero")
-    print("Ingresar dinero")
-    print("Consultar Saldo")
-    print("Cambiar pin")
+    print("1 = Retirar dinero")
+    print("2 = Ingresar dinero")
+    print("3 = Consultar Saldo")
+    print("4 = ambiar pin")
+    print("5 = Salir")
+    opcion = int(input())
 
-
-
-    break
+    if opcion == 1:
+        print()
+    elif opcion == 2:
+        print()
+    elif opcion == 3:
+        print()
+    elif opcion == 4:
+        print()
+    elif opcion == 5:
+        break
