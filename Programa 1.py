a54ccb83177a9c49274bb9c10071edba1a31b95a
JorgeLuisVG <jorgeluisvg0500@gmail.com>
2025-08-18 16:20:44 -0600

    inicio del programa

---
+++ Programa 1.py
@@ -0,0 +1,13 @@
+Usuarios = []
+print("Cajero Automatico")
+
+while True:
+    print("Ingrese una opcion")
+    print("Retirar dinero")
+    print("Ingresar dinero")
+    print("Consultar Saldo")
+    print("Cambiar pin")
+
+
+
+    break
