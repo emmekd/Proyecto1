d7b2c5e5c636865c6a54d668dba5ef6724da5614
JorgeLuisVG <jorgeluisvg0500@gmail.com>
2025-08-18 20:35:48 -0600

    correccion de errores

--- Programa 1.py
+++ Programa 1.py
@@ -2,8 +2,8 @@ Usuarios = {}
 print("Cajero Automatico")
 
 def NuevoUsuario(Nombre, ID, PIN, MontoI):
-    print(f"Nombre: {nombre}, su ID es: {id}, Pin: ****")
-    Usuarios[Nombre] = {"Id" : ID, "Pin": PIN, "Monto": MontoI}
+    print(f"Nombre: {Nombre}, su ID es: {ID}, Pin: ****")
+    Usuarios[Nombre] = {"Id": ID, "Pin": PIN, "Monto": MontoI}
 
 def IniciarSesion():
     buscar = input("ingrese el nombre de usuario").title()
@@ -41,7 +41,7 @@ while True:
     print("3 = Consultar Saldo")
     print("4 = Cambiar pin")
     print("5 = ingresar nuevo usuario")
-    print("5 = Salir")
+    print("6 = Salir")
     opcion = int(input())
 
     if opcion == 1:
@@ -53,7 +53,7 @@ while True:
     elif opcion == 3:
         ConsultarSaldo()
     elif opcion == 4:
-        NuevoPin = input("Ingrese su nuevo Pin")
+        NuevoPin = int(input("Ingrese su nuevo Pin: "))
         CambiarPin(NuevoPin)
     elif opcion == 5:
         import random
