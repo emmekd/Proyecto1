31409e6ccb4aa8d5621c7d03579e7a097455213e
Jorge Luis Villagrán González <158310235+JorgeLuisVG@users.noreply.github.com>
2025-08-26 09:56:59 -0600

    Update Clases.py

--- Clases.py
+++ Clases.py
@@ -1,3 +1,5 @@
+import random
+
 from datetime import datetime
 
 #Clase Base, Operaciones. 
@@ -48,3 +50,49 @@ class CambioPIN(Operaciones):
             raise ValueError("El pin deve ser numerico de 4 dijitos")
         self.cuenta.pin = nuevPIN
         self.cuenta.agregar_historial = (f"Cambio el PIN el {self.nuevPIN} en la fecha {self.fecha}")
+
+
+Usuarios = {}
+class Cuenta:
+    def __init__(self, Nombre, Id, Pin, Saldo):
+        self.Nombre = Nombre
+        self.Id = Id
+        self.__Pin = Pin
+        self.__Saldo = Saldo
+    def NuevoUsuario(self):
+        rand = random.randint(1000000, 9999999)
+
+        self.Id = rand
+        Usuarios[self.Nombre] = {
+            "ID": self.Id,
+            "Pin": self.__Pin,
+            "Saldo": self.__Saldo}
+    def get(self):
+        print(f"Nombre: {self.Nombre}")
+        print(f"Id: {self.Id}")
+        print(f"Saldo: {self.__Saldo}")
+    def set(self):
+        def retirar(nuevo):
+            self.__Saldo -= nuevo
+            print(f"saldo: {self.__Saldo}")
+        def depositar(nuevo):
+            self.__Saldo += nuevo
+            print(f"saldo: {self.__Saldo}")
+        while True:
+            try:
+                monto = int(input("Ingrese el monto"))
+            except ValueError:
+                print("Ingrese un monto valido")
+            if monto > 0:
+                break
+        while True:
+            try:
+                opcion = int(input("Desea depositar o retirar?(1 = R, 2 = D)"))
+            except ValueError:
+                print("ingrese una opcion valida")
+            if opcion == 1 or opcion == 2:
+                break
+        if opcion == 1:
+            retirar(monto)
+        if opcion == 2:
+            depositar(monto)
